2b27b2e469c2f97fcb22b13195699e8e94cef4e1
mjpatel <mjpatel@iastate.edu>
2023-05-03 19:25:08 -0500

    Done

--- frontend/my-app/src/pages/Services.tsx
+++ frontend/my-app/src/pages/Services.tsx
@@ -6,6 +6,16 @@ const Services: React.FC = () => {
   return (
     <div>
       <h1>Services</h1>
+      <p>
+      <Subcategory title="Repair" path="/Services/repair" /> </p>
+      <p>
+      <Subcategory title="Financial Services" path="/Services/financialservices" /> </p>
+      <p>
+      <Subcategory title="Construction" path="/Services/Construction" /> </p>
+      <p>
+      <Subcategory title="PublicRelation" path="/Services/PublicRelation" /> </p>
+      <p>
+      <Subcategory title="HealthCare" path="/Services/HealthCare" /> </p>
       
       {/* Add the rest of the subcategories */}
     </div>
